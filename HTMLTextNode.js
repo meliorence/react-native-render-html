19b600b0c0ccec6908845eb78a576fb8f554ea23
Pham The Duy <duypham.bhtech@gmail.com>
2017-08-04 14:38:07 +0700

    refactor(all): use Javascript standard style as coding convention

--- HTMLTextNode.js
+++ HTMLTextNode.js
@@ -1,17 +1,17 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { Text } from 'react-native';
-import { AllHtmlEntities } from 'html-entities';
-import { RE, TEXT_TAG_NAMES, PRE_TAG_NAMES } from './HTMLUtils';
+import { Text } from 'react-native'
+import { AllHtmlEntities } from 'html-entities'
+import { RE, TEXT_TAG_NAMES, PRE_TAG_NAMES } from './HTMLUtils'
 
 export default class HTMLTextNode extends PureComponent {
   /* ****************************************************************************/
   // Class
   /* ****************************************************************************/
 
-    static propTypes = {
-        children: PropTypes.string.isRequired
-    }
+  static propTypes = {
+    children: PropTypes.string.isRequired
+  }
 
   /**
   * Formats text the same way a browser would be removing whitespace
@@ -20,33 +20,33 @@ export default class HTMLTextNode extends PureComponent {
   * @param parentTagName: the name of the parent node
   * @return the new string
   */
-    static removeWhitespaceListHTML (str, nodeIndex, parentTagName) {
-        if (PRE_TAG_NAMES.has(parentTagName)) {
-            return str;
-        } else {
-            const htmlStr = str
-            .replace(RE.MULT_NEWLINE, '\n')
-            .replace(RE.MULT_WHITESPACE, ' ')
-            .replace(RE.PREFIX_NEWLINE, '')
-            .replace(RE.SUFFIX_NEWLINE, '');
+  static removeWhitespaceListHTML (str, nodeIndex, parentTagName) {
+    if (PRE_TAG_NAMES.has(parentTagName)) {
+      return str
+    } else {
+      const htmlStr = str
+      .replace(RE.MULT_NEWLINE, '\n')
+      .replace(RE.MULT_WHITESPACE, ' ')
+      .replace(RE.PREFIX_NEWLINE, '')
+      .replace(RE.SUFFIX_NEWLINE, '')
 
-            if (!TEXT_TAG_NAMES.has(parentTagName) && htmlStr.trim().length === 0) {
-                return '';
-            } else {
-                if (nodeIndex === 0) {
-                    return htmlStr.replace(RE.PREFIX_WHITESPACE, '');
-                } else {
-                    return htmlStr;
-                }
-            }
+      if (!TEXT_TAG_NAMES.has(parentTagName) && htmlStr.trim().length === 0) {
+        return ''
+      } else {
+        if (nodeIndex === 0) {
+          return htmlStr.replace(RE.PREFIX_WHITESPACE, '')
+        } else {
+          return htmlStr
         }
+      }
     }
+  }
 
   /* ****************************************************************************/
   // Rendering
   /* ****************************************************************************/
 
-    render () {
-        return (<Text {...this.props}>{AllHtmlEntities.decode(this.props.children)}</Text>);
-    }
+  render () {
+    return (<Text {...this.props}>{AllHtmlEntities.decode(this.props.children)}</Text>)
+  }
 }
