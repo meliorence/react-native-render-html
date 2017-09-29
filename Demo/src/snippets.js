4af8edba984681a38978849bb2349c97e5c126c3
Maxime Bertonnier <m.bertonnier@gmail.com>
2017-09-29 09:22:12 +0200

    chore(snippets): more examples & add tricky cases

--- Demo/src/snippets.js
+++ Demo/src/snippets.js
@@ -8,15 +8,35 @@ export const paragraphs = `
     <p style="padding:10%;">This one features a padding <strong>in percentage !</strong></p>
     <hr />
     <i>Here, we have a style set on the "i" tag with the "tagsStyles" prop.</i>
-    <p class="last-paragraph">Finally, this paragraph is style through the classesStyles prop</p>
+    <p>And <a href="http://google.fr">This is a link !</a></p>
+    <a href="http://google.fr"><div style="background-color: red; height: 20px; width:40px;"></div></a>
+    <p class="last-paragraph">Finally, this paragraph is styled through the classesStyles prop</p>
+`;
+
+export const lists = `
+    <p>Here is an <em>ul</em> tag</p>
+    <ul>
+        <li>Easy</li>
+        <li>Peasy</li>
+        <li><div style="background-color:red;width:50px;height:50px;"></div></li>
+        <li>Lemon</li>
+        <li>Squeezy</li>
+    </ul>
+
+    <br />
+    <p>Here is an <em>ol</em> tag</p>    
+    <ol>
+        <li>Sneaky</li>
+        <li>Beaky</li>
+        <li>Like</li>
+    </ol>
 `;
 
 export const simpleLoremWithImages = `
     <p>This first image's dimensions are set in its style attributes.</p>
     <img style="width: 50%; height: 100px; align-self: center;" src="https://i.imgur.com/gSmWCJF.jpg" />
     <p>The next image will be sized automatically thanks to the "imagesMaxWidth" prop.</p>
-    <img src="https://i.imgur.com/XP2BE7q.jpg" />    
-    <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+    <img src="https://i.imgur.com/XP2BE7q.jpg" />
 `;
 
 export const imagesWithinParagraphs = `
@@ -66,8 +86,8 @@ export const ignoringTagsAndStyles = `
     <p>You can also use a function to ignore nodes if you need to be even more specific.</p>
     <p>Let's ignore "a" tags nested inside "divs"</p>
     <p><a href="http://google.com">This link</a> works since it's nested inside a paragraph.</p>
-    <div style="backgroundColor: teal; padding: 10px; color: white; text-align: center;">
-        <a href="http://google.com">you're a noisy one, aren't you ?</a>Saw the link in this div ? It has been ignored !
+    <div style="padding: 10px; color: white; text-align: center; border-width: 1px;">
+        <a href="http://google.com">you're a noisy one, aren't you ?</a>Can you see link in this div ? It has been ignored !
     </div>
 `;
 
@@ -81,6 +101,7 @@ export const customHTMLTags = `
     <p>For our final trick, let's style this custom tag with a class.</p>
     <bluecircle class="make-me-green"></bluecircle>
     <p>That demonstrates how easily we can override styles. In ascending priority :</p>
+    Style from the class with the <em>classesStyles</em> prop (green)
     <ul>
         <li>Default style for the custom component (blue)</li>
         <li>Style from the class with the <em>classesStyles</em> prop (green)</li>
@@ -113,6 +134,7 @@ export default {
             classesStyles: { 'last-paragraph': { textAlign: 'right', color: 'teal', fontWeight: '800' } }
         }
     },
+    lists: { name: 'Lists' },
     simpleLoremWithImages: { name: 'Simple lorem (images)' },
     imagesWithinParagraphs: { name: 'Images within paragraphs' },
     images404: { name: '404 images' },
@@ -123,7 +145,7 @@ export default {
         props: {
             ignoredTags: ['h2'],
             ignoredStyles: ['background-color'],
-            ignoreNodesFunction: (node, parentTagName, parentIsText) => {
+            ignoreNodesFunction: (node, parentTagName) => {
                 return node.parent && node.parent.name === 'a' && node.parent.parent && node.parent.parent.name === 'div';
             }
         }
