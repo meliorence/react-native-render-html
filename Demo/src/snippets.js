a32f7d70590e44051e6ae6c1f1160bfb8b03d979
Maxime Bertonnier <m.bertonnier@gmail.com>
2017-10-26 11:14:29 +0200

    doc(Demo): add example demonstrating text styling behaviour

--- Demo/src/snippets.js
+++ Demo/src/snippets.js
@@ -3,14 +3,14 @@ import { View } from 'react-native';
 import { _constructStyles } from 'react-native-render-html/src/HTMLStyles';
 
 export const paragraphs = `
-    <p style="font-size:1.3em;">This paragraph is styled a font size set in em !</p>
-    <em>This one showcases the default renderer for the "em" HTML tag.</em>
-    <p style="padding:10%;">This one features a padding <strong>in percentage !</strong></p>
-    <hr />
-    <i>Here, we have a style set on the "i" tag with the "tagsStyles" prop.</i>
-    <p>And <a href="http://google.fr">This is a link !</a></p>
-    <a href="http://google.fr"><div style="background-color: red; height: 20px; width:40px;"></div></a>
-    <p class="last-paragraph">Finally, this paragraph is styled through the classesStyles prop</p>
+<p style="font-size:1.3em;">This paragraph is styled a font size set in em !</p>
+<em>This one showcases the default renderer for the "em" HTML tag.</em>
+<p style="padding:10%;">This one features a padding <strong>in percentage !</strong></p>
+<hr />
+<i>Here, we have a style set on the "i" tag with the "tagsStyles" prop.</i>
+<p>And <a href="http://google.fr">This is a link !</a></p>
+<a href="http://google.fr"><div style="background-color: red; height: 20px; width:40px;"></div></a>
+<p class="last-paragraph">Finally, this paragraph is styled through the classesStyles prop</p>
 `;
 
 export const lists = `
@@ -76,6 +76,31 @@ export const layoutStyles = `
         <p style="color:white">Text inside a rectangle with a 20% padding</p>
     </div>
 `;
+
+export const textsStylesBehaviour = `
+<p>Styling texts is a very tricky part of converting HTML into react-native components.</p>
+<p>The way react-native's <em>Text</em> components behaves is a lot different from our browsers' implementation.</p>
+<p>Let's see how styles are applied to texts with this plugin.</p>
+
+<div style="color:red;">This text is inside a div, without a text tag wrapping it. The <em>div</em> tag only has <em>color:red;</em> as style.</div>
+
+In the example above, you may find, if you inspect the rendered components, that it's the <em>Text</em> component inside that actually receives the color attribute.
+This is because this library parses every text-only style of <em>View</em> wrappers and moves them to each <em>Text</em> child.
+
+<div style="color:red">
+    <p>This first paragraph doesn't have a specific styling.</p>
+    <p style="color:blue;">This one is blue.</p>
+</div>
+
+<p>Here, the <em>div</em> wrapper still has <em>color:red;</em> as style.</div>.</p>
+
+<p>The first paragraph inside it doesn't have any style attribute, either from HTML or from the <em>tagsStyles</em> or <em>classesStyles</em> props.</p>
+<p>The second one is set to be blue from its <em>style</em> attribute.</p>
+
+<p>You can see the order of priorities that applies to styling. The less important are your <em>tagsStyles</em>, 
+then your <em>classessStyles</em> and finally the styles parsed from your HTML content.</p>
+`;
+
 
 export const ignoringTagsAndStyles = `
     <p>The following tag (h2) is ignored with the "ignoredTags" prop</p>
@@ -155,6 +180,14 @@ export default {
     images404: { name: '404 images', props: { baseFontSize: 20 } },
     trickyStuff: { name: 'Tricky stuff' },
     layoutStyles: { name: 'Layout styles' },
+    textsStylesBehaviour: {
+        name: 'Texts styles behaviour',
+        props: {
+            tagsStyles: {
+                div: { borderWidth: 1, padding: 10, marginBottom: 10 }
+            }
+        }
+    },
     ignoringTagsAndStyles: {
         name: 'Ignoring tags & styles',
         props: {
