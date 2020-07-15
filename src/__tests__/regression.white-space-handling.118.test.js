e512e7faf7cb9e64ffc8902a8a571e9f89dd1883
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-07-15 10:00:05 -0300

    tests(regression): fix jsx html prop unescaped whitespace sequences
    In JSX, a prop passed as litteral string does not interpret escaped
    sequences for special whitespace characters.

--- src/__tests__/regression.white-space-handling.118.test.js
+++ src/__tests__/regression.white-space-handling.118.test.js
@@ -6,7 +6,7 @@ import { extractTextFromInstance } from "./utils";
 describe("HTML component", () => {
   it("should pass regression #118", () => {
     const testRenderer = renderer.create(
-      <HTML html="  <div>  foo\n\nbar  baz  </div>" />
+      <HTML html={"  <div>  foo\n\nbar  baz  </div>"} />
     );
     const renderedText = extractTextFromInstance(testRenderer.root);
     expect(renderedText).toEqual("foo bar baz");
