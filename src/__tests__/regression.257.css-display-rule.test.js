f5a1071ac27e251844811630760c53a01dbe4366
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-07-17 13:07:49 -0300

    tests: refactor CSS to react native style testing into utility

--- src/__tests__/regression.257.css-display-rule.test.js
+++ src/__tests__/regression.257.css-display-rule.test.js
@@ -1,19 +1,15 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import HTML from "../index";
 import { render } from "react-native-testing-library";
+import { expectTranslatedInlineCSSToMatchObject } from "./utils";
 
 /**
  * https://github.com/archriss/react-native-render-html/issues/257
  */
 describe("HTML component", () => {
   it("should pass regression #257 regarding inline display CSS rules", () => {
-    const { getByText } = render(
-      <HTML
-        html={'<p style="display: inline-block;">hello world</p>'}
-      />
-    );
-    const text = getByText("hello world");
-    expect(StyleSheet.flatten(text.props.style)).toMatchObject({ display: 'flex' });
+    expectTranslatedInlineCSSToMatchObject({
+      cssInlineRules: "display: inline-block;",
+      render,
+      reactNativeStyle: { display: "flex" },
+    });
   });
 });
