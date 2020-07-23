675ebd61d252d7c93ef227b9cd221b8050c3698e
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-07-23 11:58:35 -0300

    tests: provide regression against #141, blur images on iOS

--- src/__tests__/regression.141.custom-blur-image.test.js
+++ src/__tests__/regression.141.custom-blur-image.test.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { Image } from "react-native";
 import { render } from "react-native-testing-library";
 import HTMLImage from "../HTMLImage";
-
-function getSize(source, callback) {
-    setTimeout(() => callback(640, 360), 1)
-}
-
-Image.getSize = getSize.bind(Image);
 
 /**
  * https://github.com/archriss/react-native-render-html/issues/141
  */
 describe("HTMLImage component should pass regression test #141", () => {
-    it("doesn't display the image prior to receiving original dimensions", () => {
-        const {} = render(<HTMLImage source={{ uri: "http://via.placeholder.com/640x360" }} />);
-    })
+  it("doesn't display the image prior to receiving original dimensions", async () => {
+    const source = { uri: "http://via.placeholder.com/640x360" };
+    const style = {};
+    const { findByTestId, getByTestId, queryByTestId } = render(
+      <HTMLImage key="1" style={style} source={source} />
+    );
+    const placeholder = getByTestId("image-placeholder");
+    expect(placeholder).toBeTruthy();
+    const imageLayout = queryByTestId("image-layout")
+    expect(imageLayout).toBeFalsy();
+    await expect(
+        findByTestId("image-layout", { timeout: 10 })
+    ).resolves.toBeTruthy();
+  });
 });
