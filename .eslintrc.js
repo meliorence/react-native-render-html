f2e70cd3ce5d0f3350fd8a2c64107cbcc534035e
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-18 11:14:41 -0300

    chore: transition repo to monorepo with yarn berry

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
-  extends: ["@react-native-community", "prettier"],
-  ignorePatterns: ["lib/"],
+  root: true,
+  extends: ['@react-native-community'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['lib/', 'types/'],
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: true }
+    ],
+    'no-eval': 'off'
+  }
 };
--- types/react-native-render-html/tests.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import HTML from "react-native-render-html";
-
-// $ExpectType Element
-const test = <HTML />;
