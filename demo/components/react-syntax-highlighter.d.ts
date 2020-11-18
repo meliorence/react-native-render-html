a2a7c6fd5c3fec31e3f41ed2fb7bd0254847a125
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-18 16:42:34 -0300

    chore(demo): adapt theming to colorscheme

--- demo/components/react-syntax-highlighter.d.ts
+++ demo/components/react-syntax-highlighter.d.ts
@@ -1,5 +1,6 @@
-declare module 'react-syntax-highlighter/styles/hljs' {
-  export const docco: any;
+declare module 'react-syntax-highlighter/styles/prism' {
+  export const darcula: any;
+  export const solarizedlight: any;
 }
 declare module 'react-native-syntax-highlighter' {
   const module: any;
