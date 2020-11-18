a69e901523e511521d441a284a7e31df89a3a51e
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-18 13:27:24 -0300

    chore(demo): add html source viewer

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -14,10 +15,10 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
+      <PaperProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
-      </SafeAreaProvider>
+      </PaperProvider>
     );
   }
 }
--- demo/navigation/index.tsx
+++ demo/navigation/index.tsx
@@ -1,13 +1,17 @@
+import * as React from 'react';
 import {
   NavigationContainer,
   DefaultTheme,
-  DarkTheme
+  DarkTheme,
+  useNavigation
 } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 import { ColorSchemeName } from 'react-native';
+import { Appbar } from 'react-native-paper';
 import snippets from './snippets';
 import Snippet from './Snippet';
+import SourceRenderer from '../components/SourceRenderer';
 
 export default function Navigation({
   colorScheme
@@ -23,23 +27,46 @@ export default function Navigation({
   );
 }
 
+const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator<Record<keyof typeof snippets, any>>();
 
 function RootNavigator() {
-  const snippetPages = Object.keys(snippets).map((snippetId) => {
-    return (
-      <Drawer.Screen
-        key={snippets[snippetId].name}
-        name={snippets[snippetId].name}>
-        {() => <Snippet exampleId={snippetId} />}
-      </Drawer.Screen>
-    );
-  });
   return (
-    <Drawer.Navigator
-      initialRouteName={'paragraphs'}
-      screenOptions={{ headerShown: false }}>
-      {snippetPages}
-    </Drawer.Navigator>
+    <Stack.Navigator initialRouteName="home">
+      <Stack.Screen name="home" options={{ headerShown: false }}>
+        {({ navigation }) => (
+          <Drawer.Navigator
+            initialRouteName={'test'}
+            hideStatusBar={false}
+            screenOptions={{ headerShown: false }}>
+            {Object.keys(snippets).map((snippetId) => {
+              return (
+                <Drawer.Screen
+                  options={{
+                    title: snippets[snippetId].name,
+                    headerShown: true,
+                    headerRight: ({ tintColor }) => (
+                      <Appbar.Action
+                        icon="xml"
+                        onPress={() =>
+                          navigation.navigate(
+                            'source',
+                            snippets[snippetId].html as any
+                          )
+                        }
+                      />
+                    )
+                  }}
+                  key={snippets[snippetId].name}
+                  name={snippets[snippetId].name}>
+                  {() => <Snippet exampleId={snippetId} />}
+                </Drawer.Screen>
+              );
+            })}
+          </Drawer.Navigator>
+        )}
+      </Stack.Screen>
+      <Stack.Screen name="source" component={SourceRenderer} />
+    </Stack.Navigator>
   );
 }
