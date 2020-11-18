9c7367dbf46c21db105ebcc4605b0890b42939c6
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-18 17:23:16 -0300

    chore(demo): fix source renderer style

--- demo/components/SourceRenderer.tsx
+++ demo/components/SourceRenderer.tsx
@@ -1,11 +1,23 @@
 import { StackScreenProps } from '@react-navigation/stack';
-import React from 'react';
-import { useColorScheme } from 'react-native';
+import React, { PropsWithChildren } from 'react';
+import { useColorScheme, View, ViewProps, ViewStyle } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import SyntaxHighlighter from 'react-native-syntax-highlighter';
 import { solarizedlight, darcula } from 'react-syntax-highlighter/styles/prism';
 
 export interface SourceRenderer {
   htmlSource: string;
+}
+
+function Container({ children, style }: PropsWithChildren<ViewProps>) {
+  return (
+    <ScrollView
+      horizontal
+      style={{ flexGrow: 1 }}
+      contentContainerStyle={[style, { margin: 0, flexGrow: 1 }]}>
+      {children}
+    </ScrollView>
+  );
 }
 
 export default function SourceRenderer({
@@ -19,6 +31,7 @@ export default function SourceRenderer({
       language="html"
       fontSize={12}
       highlighter="prism"
+      PreTag={Container}
       style={colorMode === 'dark' ? darcula : solarizedlight}>
       {html}
     </SyntaxHighlighter>
