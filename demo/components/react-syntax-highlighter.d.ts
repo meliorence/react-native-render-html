caed90759239ac186ea38ec8fa1f9ac7cee0c421
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-18 13:27:24 -0300

    chore(demo): add html source viewer

---
+++ demo/components/react-syntax-highlighter.d.ts
@@ -0,0 +1,7 @@
+declare module 'react-syntax-highlighter/styles/hljs' {
+  export const docco: any;
+}
+declare module 'react-native-syntax-highlighter' {
+  const module: any;
+  export default module;
+}
