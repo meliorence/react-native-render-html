dd1cf47fc9c2e68f9a2104294c998313dfc2b1cd
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-20 16:24:28 -0300

    chore(demo): add a version banner

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -13,10 +14,10 @@ export default function App() {
     return null;
   } else {
     return (
-      <>
+      <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
-      </>
+      </SafeAreaProvider>
     );
   }
 }
