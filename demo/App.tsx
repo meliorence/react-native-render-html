38c3946ff0baeefa6e20b337361b3ea70e6c2334
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-21 12:28:01 -0300

    chore(demo): circumvent white flickering in dark mode

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,3 +1,4 @@
+import { enableScreens } from 'react-native-screens';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -5,6 +6,8 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+enableScreens();
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
