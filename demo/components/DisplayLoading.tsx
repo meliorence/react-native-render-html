d495793fe1805ccc584927ac033f1afdaac35386
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-22 15:04:15 -0300

    chore(demo): set accent color to DisplayLoading

--- demo/components/DisplayLoading.tsx
+++ demo/components/DisplayLoading.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
 import { ActivityIndicator } from 'react-native-paper';
+import { useComponentColors } from '../state/ThemeProvider';
 
 export default function DisplayLoading() {
+  const {
+    displayLoading: { color }
+  } = useComponentColors();
   return (
     <View
       style={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <ActivityIndicator size="large" />
+      <ActivityIndicator color={color} size="large" />
     </View>
   );
 }
