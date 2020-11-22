d04ba232d68852fe8828eafa7411aa427dd1e923
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-22 06:53:21 -0300

    chore(demo): wip zustand

--- demo/state/TTreeContextProvider.ts
+++ demo/state/TTreeContextProvider.ts
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import { TNode } from '@native-html/transient-render-engine';
 
-const TTreeContext = React.createContext<{ ttree: TNode | null, setTTree: (ttree: TNode) => void; }>({ ttree: null, setTTree: () => null });
+const TTreeContext = React.createContext<{
+  ttree: TNode | null;
+  setTTree: (ttree: TNode) => void;
+}>({ ttree: null, setTTree: () => null });
 export const useTTree = () => React.useContext(TTreeContext);
-const TTreeContextProvider = TTreeContext.Provider
+const TTreeContextProvider = TTreeContext.Provider;
 
 export default TTreeContextProvider;
