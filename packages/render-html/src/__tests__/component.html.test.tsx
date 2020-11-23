1114166a8b373fd5e986fd67ada35a57fee0d743
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-23 10:55:19 -0300

    refactor: move tags to elements

--- packages/render-html/src/__tests__/component.html.test.tsx
+++ packages/render-html/src/__tests__/component.html.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-native-testing-library';
 import HTML from '../RenderHTML';
-import ImgTag from '../tags/ImgTag';
+import ImgTag from '../elements/HTMLImgElement';
 
 describe('ImgTag', () => {
   it('should update ImgTag contentWidth when contentWidth prop changes', () => {
