51b83430048d5cae08da3d47895de66ada1f2bb7
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-25 20:22:28 -0300

    style: fix lint error

--- packages/render-html/src/context/TChildrenRendererContext.ts
+++ packages/render-html/src/context/TChildrenRendererContext.ts
@@ -4,9 +4,9 @@ import type TNodeChildrenRenderer from '../TNodeChildrenRenderer';
 /**
  * This context allows dependency injection to avoid circular dependencies.
  */
-const TChildrenRenderersContext = React.createContext<typeof TNodeChildrenRenderer>(
-  () => null
-);
+const TChildrenRenderersContext = React.createContext<
+  typeof TNodeChildrenRenderer
+>(() => null);
 
 export function useTChildrenRenderer() {
   return React.useContext(TChildrenRenderersContext);
