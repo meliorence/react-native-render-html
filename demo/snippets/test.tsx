9d3bfa673564f33e73862b612c72945115cdc14d
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-26 10:26:32 -0300

    chore(demo): add CTA in the HTML content

--- demo/snippets/test.tsx
+++ demo/snippets/test.tsx
@@ -2,7 +2,9 @@ import { SnippetDeclaration } from '../types';
 
 // Change the HTML code here and see what happens.
 const html = `
-Testing playground.
+<p>
+  Open <code>demo/snippets/test.tsx</code> and edit me!
+</p>
 `;
 
 const test: SnippetDeclaration = {
