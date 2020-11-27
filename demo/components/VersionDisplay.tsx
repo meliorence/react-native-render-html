74c14f136a9aba9ae66673e4756b5d39e5f598a6
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-27 14:21:25 -0300

    chore(demo): display react-native-render-html version

--- demo/components/VersionDisplay.tsx
+++ demo/components/VersionDisplay.tsx
@@ -1,28 +1,30 @@
 /* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
-import { View } from 'react-native';
+import { TextStyle, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 // @ts-ignore
 import version from '../version';
 import MonoText from './MonoText';
+
+const monoStyle: TextStyle = {
+  fontSize: 10,
+  textAlign: 'left'
+};
 
 export default function VersionDisplay() {
   const { bottom, left, right } = useSafeAreaInsets();
   return (
     <View
       style={{
-        alignSelf: 'stretch'
+        alignSelf: 'stretch',
+        marginBottom: bottom,
+        marginLeft: left,
+        marginRight: right,
+        padding: 10
       }}>
-      <MonoText
-        style={{
-          fontSize: 10,
-          padding: 10,
-          textAlign: 'left',
-          marginBottom: bottom,
-          marginLeft: left,
-          marginRight: right
-        }}>
-        Foundry Playground v{version}
+      <MonoText style={monoStyle}>Foundry Playground {version.demo}</MonoText>
+      <MonoText style={monoStyle}>
+        react-native-render-html {version.lib}
       </MonoText>
     </View>
   );
