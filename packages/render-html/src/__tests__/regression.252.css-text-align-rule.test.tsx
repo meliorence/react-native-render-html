5a06cea0f21dcadf5467b5f1c7ab8210fad4851f
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-29 10:22:17 -0300

    test: backport regression tests from legacy

---
+++ packages/render-html/src/__tests__/regression.252.css-text-align-rule.test.tsx
@@ -0,0 +1,29 @@
+import { render } from 'react-native-testing-library';
+import { expectTranslatedInlineCSSValueToEqual } from './utils';
+
+function testTranslation(cssValue: string, rnValue: any) {
+  it(`sould translate ${cssValue} value to ${rnValue}`, () => {
+    expectTranslatedInlineCSSValueToEqual({
+      cssInlineRules: `text-align: ${cssValue};`,
+      render,
+      reactNativePropStyleName: 'textAlign',
+      value: rnValue
+    });
+  });
+}
+
+/**
+ * https://github.com/archriss/react-native-render-html/issues/252
+ */
+describe('HTML component', () => {
+  describe('should pass regression #252 regarding inline text-align CSS rules', () => {
+    testTranslation('left', 'left');
+    testTranslation('right', 'right');
+    testTranslation('auto', 'auto');
+    testTranslation('justify', 'justify');
+    testTranslation('center', 'center');
+    testTranslation('start', undefined);
+    testTranslation('end', undefined);
+    testTranslation('justify-all', undefined);
+  });
+});
---
+++ packages/render-html/src/__tests__/regression.343.tags-styles-re-render.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import RenderHTML from '../RenderHTML';
+import { render } from 'react-native-testing-library';
+
+/**
+ * https://github.com/archriss/react-native-render-html/issues/343
+ */
+describe('RenderHTML component', () => {
+  const letterSpacing2 = {
+    letterSpacing: 2
+  };
+  const letterSpacing3 = {
+    letterSpacing: 3
+  };
+  const tagsStylesInstance1 = {
+    a: letterSpacing2
+  };
+  const tagsStylesInstance2 = {
+    a: letterSpacing3
+  };
+  it('should pass regression #343 regarding tagsStyles prop (requires triggerTREInvalidationPropNames)', () => {
+    const { getByText, update } = render(
+      <RenderHTML
+        debug={false}
+        html={'<a>hello world</a>'}
+        tagsStyles={tagsStylesInstance1}
+        triggerTREInvalidationPropNames={['tagsStyles']}
+      />
+    );
+    update(
+      <RenderHTML
+        debug={false}
+        html={'<a>hello world</a>'}
+        tagsStyles={tagsStylesInstance2}
+        triggerTREInvalidationPropNames={['tagsStyles']}
+      />
+    );
+    const text = getByText('hello world');
+    expect(StyleSheet.flatten(text.props.style)).toMatchObject(letterSpacing3);
+  });
+});
