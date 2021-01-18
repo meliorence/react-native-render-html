7a23a5e1a35425c1e92f19f8aec8b8d3ab2fae7a
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-01-18 07:07:28 -0300

    feat: safe typeguards in extendDefaultRenderer

--- packages/render-html/src/render/defaultRenderers.ts
+++ packages/render-html/src/render/defaultRenderers.ts
@@ -2,9 +2,8 @@ import ARenderer from '../renderers/ARenderer';
 import IMGRenderer from '../renderers/IMGRenderer';
 import OLRenderer from '../renderers/OLRenderer';
 import ULRenderer from '../renderers/ULRenderer';
-import { DefaultTagRendererRecord } from './render-types';
 
-const defaultRenderers: DefaultTagRendererRecord = {
+const defaultRenderers = {
   img: IMGRenderer,
   ul: ULRenderer,
   ol: OLRenderer,
