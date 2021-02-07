60c5b67e48233c1fef4cdbf0506e3b53f3ba558f
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-02-07 15:47:33 -0300

    refactor: accurate typings for HTMLAttributes

--- packages/render-html/src/shared-types.ts
+++ packages/render-html/src/shared-types.ts
@@ -35,7 +35,7 @@ export interface ImageDimensions {
 }
 
 export interface HtmlAttributesDictionary {
-  [attribute: string]: string | number;
+  [attribute: string]: string;
 }
 
 export interface RenderHTMLPassedProps<P = any> {
@@ -418,9 +418,9 @@ export type DefaultTagRendererProps<T extends TNode> = TRendererBaseProps<T> & {
   TDefaultRenderer: TDefaultRenderer<T>;
 };
 
-export type CustomTagRendererProps<
-  T extends TNode
-> = DefaultTagRendererProps<T> & {
+export type CustomTagRendererProps<T extends TNode> = DefaultTagRendererProps<
+  T
+> & {
   /**
    * Internal renderer for this _tagName_, not to be confused with
    * {@link TDefaultRenderer}, which is the default renderer for the _tnode_.
