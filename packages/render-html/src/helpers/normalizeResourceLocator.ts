357b8b53c3631f7b73e313cba31c8c1f8b1ea4a1
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-02-08 16:12:46 -0300

    fix: use urijs instead of URL API
    A react-native bug adds an extraneous slash to URL.href, causing some
    resources such as images to misload.
    
    fix #460

--- packages/render-html/src/helpers/normalizeResourceLocator.ts
+++ packages/render-html/src/helpers/normalizeResourceLocator.ts
@@ -1,17 +1,19 @@
+import URI from 'urijs';
+
 /**
  * This function normalize relative and protocol-relative URLs to absolute
  * URLs as per {@link https://tools.ietf.org/html/rfc1808 RFC1808}.
  *
- * @param resource - The URL to normalize.
+ * @param url - The URL to normalize.
  * @param baseUrl - The base URL to resolve relative and protocol-relative URLs.
  */
 export default function normalizeResourceLocator(
-  resource: string,
-  baseUrl: string
+  url: string,
+  baseUrl?: string
 ) {
   try {
-    return new URL(resource, baseUrl).href;
+    return baseUrl ? URI(url).absoluteTo(URI(baseUrl)).href() : URI(url).href();
   } catch (e) {
-    return resource;
+    return url;
   }
 }
