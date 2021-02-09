0c0a9c64f415bfb7228a64cb3d450d1168bd1a01
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-02-09 14:02:52 -0300

    feat: reeport useful variables and types from transient-render-engine

--- packages/render-html/src/index.ts
+++ packages/render-html/src/index.ts
@@ -1,9 +1,41 @@
 import RenderHTML from './RenderHTML';
 export {
-  HTMLContentModel,
-  TStyles,
   defaultHTMLElementModels,
-  tnodeToString
+  DOMElement,
+  DOMNode,
+  DOMText,
+  HTMLContentModel,
+  isSerializableElement,
+  isSerializableText,
+  TBlock,
+  TDocument,
+  TEmpty,
+  TNode,
+  tnodeToString,
+  toSerializableNode,
+  TPhrasing,
+  TRenderEngine,
+  TStyles
+} from '@native-html/transient-render-engine';
+export type {
+  AnchorTagName,
+  AttribTagNames,
+  DefaultHTMLElementModels,
+  EditsTagNames,
+  EmbeddedTagNames,
+  GroupingTagNames,
+  InteractiveTagNames,
+  MetadataTagNames,
+  SectioningTagNames,
+  SerializableElement,
+  SerializableNode,
+  SerializableText,
+  TabularTagNames,
+  TagName,
+  TextLevelTagNames,
+  TRenderEngineOptions,
+  UnsupportedTagNames,
+  UntranslatableTagNames
 } from '@native-html/transient-render-engine';
 export * from './shared-types';
 export * from './render/render-types';
@@ -16,10 +48,10 @@ export {
 } from './TNodeChildrenRenderer';
 export type { TNodeChildrenRendererProps } from './TNodeChildrenRenderer';
 export { default as TNodeRenderer } from './TNodeRenderer';
-export { default as useTRenderEngine } from './hooks/useTRenderEngine';
-export { default as useTTree } from './hooks/useTTree';
 export { default as useInternalRenderer } from './hooks/useInternalRenderer';
 export { default as useNormalizedUrl } from './hooks/useNormalizedUrl';
+export { default as useTRenderEngine } from './hooks/useTRenderEngine';
+export { default as useTTree } from './hooks/useTTree';
 export type {
   InternalSpecialRenderedTag,
   InternalRendererConfig
@@ -28,9 +60,9 @@ export { default as defaultRenderers } from './render/defaultRenderers';
 export { default as extendDefaultRenderer } from './render/extendDefaultRenderer';
 export { default as splitBoxModelStyle } from './helpers/splitBoxModelStyle';
 export {
-  useSharedProps,
+  useComputeMaxWidthForTag,
   useRendererProps,
-  useComputeMaxWidthForTag
+  useSharedProps
 } from './context/SharedPropsContext';
 export { useDocumentMetadata } from './context/DocumentMetadataProvider';
 export { default as domNodeToHTMLString } from './helpers/domNodeToHTMLString';
