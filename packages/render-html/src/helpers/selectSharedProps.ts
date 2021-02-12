1f9fbc9752d908849ce0daedb8084ac00f2f548d
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-02-12 13:37:43 -0300

    refactor: accurate typings for `RenderHTMLSharedProps`, `useSharedProps`
    BREAKING CHANGE: (typescript users) renamed `RenderHTMLPassedProps` to
    `RenderHTMLSharedProps` for consistency. In addition, changed signature
    of `useSharedProps` and `userRendererProps`. These hooks now take as
    first generic type arugment the shape of `renderersProps`.

--- packages/render-html/src/helpers/selectSharedProps.ts
+++ packages/render-html/src/helpers/selectSharedProps.ts
@@ -2,12 +2,12 @@ import pickBy from 'ramda/src/pickBy';
 import pick from 'ramda/src/pick';
 import pipe from 'ramda/src/pipe';
 import mergeRight from 'ramda/src/mergeRight';
-import { RenderHTMLPassedProps, RenderHTMLProps } from '../shared-types';
+import { RenderHTMLSharedProps, RenderHTMLProps } from '../shared-types';
 import { defaultSharedPropsContext } from '../context/SharedPropsContext';
 
 const selectSharedProps: (
   props: Partial<RenderHTMLProps>
-) => Required<RenderHTMLPassedProps> = pipe(
+) => Required<RenderHTMLSharedProps> = pipe(
   pick(Object.keys(defaultSharedPropsContext)),
   pickBy((val) => val != null),
   mergeRight(defaultSharedPropsContext) as any
