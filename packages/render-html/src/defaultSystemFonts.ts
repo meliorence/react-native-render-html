ebece1d7ef9950c7c1fb1407cac07b7bd3f67c05
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-02-14 11:48:58 -0300

    feat: support default system fonts for Windows, Macos and web

---
+++ packages/render-html/src/defaultSystemFonts.ts
@@ -0,0 +1,7 @@
+/**
+ * Default system fonts based on current platform.
+ *
+ * @remarks If you are using Expo, use `Constants.systemFonts` instead.
+ */
+const defaultSystemFonts = ['Arial', 'Courier New', 'Georgia'];
+export default defaultSystemFonts;
