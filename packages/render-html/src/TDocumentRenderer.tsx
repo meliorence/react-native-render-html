b5dffea5ea9ce07f65aac1713b8202c3a9498c3d
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-02-20 10:47:45 -0300

    perf: memoize `TDocumentRenderer` to prevent rendering from ascendents

--- packages/render-html/src/TDocumentRenderer.tsx
+++ packages/render-html/src/TDocumentRenderer.tsx
@@ -1,55 +1,57 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { memo, useEffect, useMemo } from 'react';
 import { TDocument } from '@native-html/transient-render-engine';
 import { DocumentMetadata, RenderHTMLFragmentProps } from './shared-types';
 import DocumentMetadataProvider from './context/DocumentMetadataProvider';
 import { useTNodeChildrenRenderer } from './context/TChildrenRendererContext';
 import { defaultMarkers } from './helpers/getMarkersFromTNode';
 
-const TDocumentRenderer = ({
-  tdoc,
-  baseUrl,
-  onDocumentMetadataLoaded
-}: {
-  tdoc: TDocument;
-  baseUrl?: string;
-  onDocumentMetadataLoaded?: RenderHTMLFragmentProps['onDocumentMetadataLoaded'];
-}) => {
-  const TNodeChildrenRenderer = useTNodeChildrenRenderer();
-  const metadata: DocumentMetadata = useMemo(() => {
-    const {
-      baseHref,
-      baseTarget,
-      lang,
-      links,
-      meta,
-      title,
-      dir
-    } = tdoc.context;
-    return {
-      baseTarget,
-      baseUrl: baseUrl ?? baseHref,
-      lang,
-      dir,
-      links,
-      meta,
-      title
-    };
-  }, [tdoc.context, baseUrl]);
-  useEffect(() => {
-    onDocumentMetadataLoaded?.call(null, metadata);
-  }, [onDocumentMetadataLoaded, metadata]);
-  const parentMarkers = useMemo(
-    () => ({
-      ...defaultMarkers,
-      direction: metadata.dir,
-      lang: metadata.lang
-    }),
-    [metadata.dir, metadata.lang]
-  );
-  return (
-    <DocumentMetadataProvider value={metadata}>
-      <TNodeChildrenRenderer parentMarkers={parentMarkers} tnode={tdoc} />
-    </DocumentMetadataProvider>
-  );
-};
+const TDocumentRenderer = memo(
+  ({
+    tdoc,
+    baseUrl,
+    onDocumentMetadataLoaded
+  }: {
+    tdoc: TDocument;
+    baseUrl?: string;
+    onDocumentMetadataLoaded?: RenderHTMLFragmentProps['onDocumentMetadataLoaded'];
+  }) => {
+    const TNodeChildrenRenderer = useTNodeChildrenRenderer();
+    const metadata: DocumentMetadata = useMemo(() => {
+      const {
+        baseHref,
+        baseTarget,
+        lang,
+        links,
+        meta,
+        title,
+        dir
+      } = tdoc.context;
+      return {
+        baseTarget,
+        baseUrl: baseUrl ?? baseHref,
+        lang,
+        dir,
+        links,
+        meta,
+        title
+      };
+    }, [tdoc.context, baseUrl]);
+    useEffect(() => {
+      onDocumentMetadataLoaded?.call(null, metadata);
+    }, [onDocumentMetadataLoaded, metadata]);
+    const parentMarkers = useMemo(
+      () => ({
+        ...defaultMarkers,
+        direction: metadata.dir,
+        lang: metadata.lang
+      }),
+      [metadata.dir, metadata.lang]
+    );
+    return (
+      <DocumentMetadataProvider value={metadata}>
+        <TNodeChildrenRenderer parentMarkers={parentMarkers} tnode={tdoc} />
+      </DocumentMetadataProvider>
+    );
+  }
+);
 export default TDocumentRenderer;
