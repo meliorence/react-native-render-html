d9078a630d081de6cc88048afdb5fbfecb9c12fd
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-03-24 08:45:24 -0300

    chore(demo): rename AtomicRadioButton to AtomicRadioControl

--- demo/components/AtomicRadioControl.tsx
+++ demo/components/AtomicRadioControl.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
 import { RadioButton } from 'react-native-paper';
-import AtomicBox from '../components/AtomicBox';
+import AtomicBox from './AtomicBox';
 import useAtomicTextStyle, {
   AtomicTextStyle
 } from '../hooks/useAtomicTextStyle';
