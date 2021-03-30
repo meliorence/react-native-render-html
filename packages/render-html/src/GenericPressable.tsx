a29468de6422d916dfcfa44f04c5f43c519f776a
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-03-29 21:53:57 -0300

    feat: `GenericPressable` prop to customize the component wrapping interactive views
    Also, drop `Pressable` in favor of `TouchableNativeFeedback` and
    `TouchableHighlight` for RN retro-compatibility.
    
    Fix #472

--- packages/render-html/src/GenericPressable.tsx
+++ packages/render-html/src/GenericPressable.tsx
@@ -1,40 +1,51 @@
 import React, { PropsWithChildren } from 'react';
 import {
-  StyleSheet,
-  Pressable,
-  PressableProps,
-  StyleProp,
-  Platform
+  Platform,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  View
 } from 'react-native';
 import { DEFAULT_PRESSABLE_RIPPLE_COLOR } from './constants';
-
-const styles = StyleSheet.create({
-  pressed: {
-    opacity: Platform.select({ default: 0.8 })
-  }
-});
-
-export interface GenericPressableProps extends PressableProps {
-  style?: StyleProp<any>;
-  borderless?: boolean;
-}
+import { useSharedProps } from './context/SharedPropsProvider';
+import { GenericPressableProps } from './shared-types';
 
 export default function GenericPressable({
-  onPress,
   style,
   children,
-  hitSlop,
   borderless = false,
   ...otherProps
 }: PropsWithChildren<GenericPressableProps>) {
+  const {
+    pressableHightlightColor = DEFAULT_PRESSABLE_RIPPLE_COLOR,
+    GenericPressable: UserProvidedPressable
+  } = useSharedProps();
+  if (UserProvidedPressable) {
+    return (
+      <UserProvidedPressable style={style} {...(otherProps as any)}>
+        {children}
+      </UserProvidedPressable>
+    );
+  }
+  if (Platform.OS === 'android') {
+    return (
+      <TouchableNativeFeedback
+        useForeground
+        background={TouchableNativeFeedback.Ripple(
+          pressableHightlightColor,
+          borderless
+        )}
+        style={style}
+        {...(otherProps as any)}>
+        <View>{children}</View>
+      </TouchableNativeFeedback>
+    );
+  }
   return (
-    <Pressable
-      android_ripple={{ borderless, color: DEFAULT_PRESSABLE_RIPPLE_COLOR }}
-      style={({ pressed }) => [style, pressed && styles.pressed]}
-      onPress={onPress}
-      hitSlop={hitSlop}
-      {...otherProps}>
-      {children}
-    </Pressable>
+    <TouchableHighlight
+      underlayColor={pressableHightlightColor}
+      style={style}
+      {...(otherProps as any)}>
+      <View>{children}</View>
+    </TouchableHighlight>
   );
 }
