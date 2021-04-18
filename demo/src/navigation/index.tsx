c2fe128126f0849b9f721106b26b48dd5e8fe98c
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-04-18 18:55:19 -0300

    chore(demo): minimal support for deep linking

--- demo/src/navigation/index.tsx
+++ demo/src/navigation/index.tsx
@@ -2,25 +2,17 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { View } from 'react-native';
 import RootNavigator from './RootNavigator';
-import { useSetSelectedSnippetId } from '../state/store';
-import { SnippetId } from '../../snippets';
 import { useTheme } from '../theme/ThemeProvider';
 
+const linking = {
+  prefixes: ['https://meliorence.github.io/react-native-render-html']
+};
+
 export default function Navigation() {
-  const setSelectedSnippet = useSetSelectedSnippetId();
   const theme = useTheme();
   return (
     <View style={{ flex: 1 }}>
-      <NavigationContainer
-        // linking={LinkingConfiguration}
-        onStateChange={(s) => {
-          // FIXME: this is a hack!
-          const drawer = s?.routes[0];
-          const index = drawer!.state?.index as number;
-          const snippet = drawer!.state?.routeNames![index] as SnippetId;
-          setSelectedSnippet(snippet);
-        }}
-        theme={theme}>
+      <NavigationContainer linking={linking} theme={theme}>
         <RootNavigator />
       </NavigationContainer>
     </View>
