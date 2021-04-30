24b5f832fc7c10448e7f461af9f9da99564b333e
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-04-30 16:37:27 -0300

    chore(website): support scss

--- apps/website/src/components/HomepageFeatures.tsx
+++ apps/website/src/components/HomepageFeatures.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+import styles from './HomepageFeatures.module.scss';
 
 const FeatureList = [
   {
