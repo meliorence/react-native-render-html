7c764f5357d985f855f04d8ef5338553f2281ee9
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-05-14 15:18:03 -0300

    chore(docs): document TRE and custom rendering

---
+++ doc-tools/doc-pages/src/pages/cards/bluecircleConfig.tsx
@@ -0,0 +1,42 @@
+import { HTMLContentModel, HTMLElementModel } from 'react-native-render-html';
+import { UIRenderHtmlCardProps } from '../../toolkit/toolkit-types';
+
+const html = '<bluecircle></bluecircle>';
+
+const bluecircleConfig: UIRenderHtmlCardProps = {
+  title: 'A Custom Tag Example',
+  caption: 'A Custom Tag rendered by defining an element model for this tag.',
+  props: {
+    source: { html },
+    customHTMLElementModels: {
+      bluecircle: HTMLElementModel.fromCustomModel({
+        tagName: 'bluecircle',
+        mixedUAStyles: {
+          width: 50,
+          height: 50,
+          borderRadius: 25,
+          alignSelf: 'center',
+          backgroundColor: 'blue'
+        },
+        contentModel: HTMLContentModel.block
+      })
+    }
+  },
+  exprSrcMap: {
+    customHTMLElementModels: `{
+  bluecircle: HTMLElementModel.fromCustomModel({
+    tagName: 'bluecircle',
+    mixedUAStyles: {
+      width: 50,
+      height: 50,
+      borderRadius: 25,
+      alignSelf: 'center',
+      backgroundColor: 'blue'
+    },
+    contentModel: HTMLContentModel.block
+  })
+}`
+  }
+};
+
+export default bluecircleConfig;
