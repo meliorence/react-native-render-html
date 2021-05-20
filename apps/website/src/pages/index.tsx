22bcc34d39311ce2131b968754444ea990164c63
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-05-20 15:44:20 -0300

    chore(website): write migration guide

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.scss';
 import HomepageHeader from '../components/HomepageHeader';
 import HomepageFeatures from '../components/HomepageFeatures';
 
@@ -9,6 +11,10 @@ export default function Home() {
   return (
     <Layout title={siteConfig.title} description={siteConfig.tagline}>
       <HomepageHeader />
+      <div className={styles.annoucement}>
+        Coming from v4 or v5 ? Check out our{' '}
+        <Link to="/docs/migration-guide">migration guide</Link>.
+      </div>
       <main>
         <HomepageFeatures />
       </main>
