978cc01ed40f4595a4a9a25d2a29a7cba4296111
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-05-22 14:03:26 -0300

    chore(website): support font loading in expo snippet

--- doc-tools/doc-pages/src/pages/cards/fontSelectionSpaceMonoConfig.tsx
+++ doc-tools/doc-pages/src/pages/cards/fontSelectionSpaceMonoConfig.tsx
@@ -9,27 +9,45 @@ const html = `<p style="font-family: 'space-mono'; padding: 10px;">
   ex ea commodo consequat. 
 </p>`;
 
+const fontLoaderSrc = `function FontLoader({ children }){
+  const [fontsLoaded] = useFonts({
+    'space-mono': SpaceMono_400Regular,
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+  return <>{children}</>;
+}`;
+
 const fontSelectionSpaceMonoConfig: UIRenderHtmlCardProps = {
   title: 'Space Mono Font Selection',
   caption:
-    'This paragraph font family is set to "Space Mono", which is loaded in this project and registered via systemFonts prop.',
+    'This paragraph font family is set to "Space Mono", which is loaded with Expo font loading helpers and registered via systemFonts prop.',
   props: {
     source: {
       html
     },
     enableExperimentalMarginCollapsing: true,
-    systemFonts: ["'space-mono'", ...defaultSystemFonts]
+    systemFonts: ['space-mono', ...defaultSystemFonts]
   },
   config: {
     importStatements: [
       {
         package: 'react-native-render-html',
         named: ['defaultSystemFonts']
+      },
+      {
+        package: '@expo-google-fonts/space-mono',
+        named: ['useFonts', 'SpaceMono_400Regular']
       }
     ],
+    fnSrcMap: {
+      FontLoader: fontLoaderSrc
+    },
     exprSrcMap: {
       systemFonts: '["\'space-mono\'", ...defaultSystemFonts]'
-    }
+    },
+    wrapperComponent: 'FontLoader'
   },
   preferHtmlSrc: false
 };
