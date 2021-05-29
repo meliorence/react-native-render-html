996b652f1ceb0d49390da09e9a0bfce401a16097
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-05-29 14:17:37 -0300

    chore(docs): refinements

--- doc-tools/doc-pages/src/pages/PageContentImages.tsx
+++ doc-tools/doc-pages/src/pages/PageContentImages.tsx
@@ -29,6 +29,26 @@ const unreachableExample = `<img
   src="http://example.tld/image.jpg"
 />`;
 
+// TODO document attributes
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const imgAttributes = {
+  alt: true,
+  src: true,
+  width: true,
+  height: true,
+  crossorigin: false,
+  anonymous: false,
+  'use-credentials': false,
+  decoding: false,
+  ismap: false,
+  loading: false,
+  referrerpolicy: false,
+  sizes: false,
+  srcset: false,
+  usemap: false
+};
+
+
 export default function PageContentImages() {
   const {
     Acronym,
