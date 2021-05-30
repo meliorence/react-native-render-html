ab5b101f9241cbd8332c2fc70d4f10c58ea23248
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-05-30 10:45:55 -0300

    chore(demo): ui refinements

--- apps/demo/src/svgAssetsIndex.ts
+++ apps/demo/src/svgAssetsIndex.ts
@@ -1,8 +1,10 @@
 import { SvgAssetType } from '@doc/pages';
 import DataFlow from '../assets/svg/data-flow.svg';
+import Logo from '../assets/svg/logo.svg';
 
-const svgAssetsIndex: Record<SvgAssetType, typeof DataFlow> = {
-  'data-flow': DataFlow
+const svgAssetsIndex: Record<SvgAssetType | 'logo', typeof DataFlow> = {
+  'data-flow': DataFlow,
+  logo: Logo
 };
 
 export default svgAssetsIndex;
