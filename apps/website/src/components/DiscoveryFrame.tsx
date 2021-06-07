f82118dece347a987474877c48dd6cde516a5fe5
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-06-07 19:09:47 -0300

    chore(website): refine announcement article

--- apps/website/src/components/DiscoveryFrame.tsx
+++ apps/website/src/components/DiscoveryFrame.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 //@ts-ignore
 import discoveryVideoUrl from '@site/static/video/discovery.webm';
 import styles from './DiscoveryFrame.module.scss';
+import clsx from 'clsx';
 
 function DeviceContainer({ children, scale = 1 }) {
   return (
@@ -18,17 +19,39 @@ function DeviceContainer({ children, scale = 1 }) {
 }
 
 export default function DiscoveryFrame({ scale }: { scale?: number }) {
+  const videoRef = useRef<HTMLVideoElement>();
+  const playPause = useCallback(() => {
+    if (videoRef.current?.paused) {
+      videoRef.current?.play();
+    } else {
+      videoRef.current?.pause();
+    }
+  }, []);
   return (
-    <DeviceContainer scale={scale}>
-      <video
-        controls={false}
-        loop
-        autoPlay
-        muted
-        preload="lazy"
-        className={styles['device-content']}>
-        <source src={discoveryVideoUrl} type="video/webm" />
-      </video>
-    </DeviceContainer>
+    <div className={styles['container']}>
+      <DeviceContainer scale={scale}>
+        <video
+          ref={videoRef}
+          controls={false}
+          loop
+          autoPlay
+          muted
+          preload="lazy"
+          onClick={playPause}
+          className={styles['device-content']}>
+          <source src={discoveryVideoUrl} type="video/webm" />
+        </video>
+      </DeviceContainer>
+      <button
+        onClick={playPause}
+        className={clsx('button', 'button--primary', styles.button)}>
+        <svg width="32" height="32" viewBox="0 0 24 24">
+          <path
+            fill="var(--ifm-button-color)"
+            d="M3,5V19L11,12M13,19H16V5H13M18,5V19H21V5"
+          />
+        </svg>
+      </button>
+    </div>
   );
 }
