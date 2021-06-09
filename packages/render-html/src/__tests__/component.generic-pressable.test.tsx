977b48001091361381aee3531541c088602279a0
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-06-09 09:09:06 -0300

    test: add render GenericPressable test

---
+++ packages/render-html/src/__tests__/component.generic-pressable.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { render } from 'react-native-testing-library';
+import GenericPressable from '../GenericPressable';
+import RenderHTMLConfigProvider from '../RenderHTMLConfigProvider';
+import TRenderEngineProvider from '../TRenderEngineProvider';
+
+describe('GenericPressable', () => {
+  it('should render TouchableHighlight by default', () => {
+    render(<GenericPressable />);
+  });
+  it('should use provided GenericPressable', () => {
+    const CustomGenericPressable = jest.fn(() => null);
+    render(
+      <TRenderEngineProvider>
+        <RenderHTMLConfigProvider GenericPressable={CustomGenericPressable}>
+          <GenericPressable />
+        </RenderHTMLConfigProvider>
+      </TRenderEngineProvider>
+    );
+    expect(CustomGenericPressable).toHaveBeenCalled();
+  });
+  it('should render TouchableNativeFeedback on Android', () => {
+    Platform.OS = 'android';
+    render(<GenericPressable />);
+  });
+});
