4fb84ae9238afdc4ad456ca471c25161a7e3d884
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-06-10 11:18:18 -0300

    refactor: move fallback WebView to WebViewPlaceholder

--- packages/render-html/src/context/defaultSharedProps.ts
+++ packages/render-html/src/context/defaultSharedProps.ts
@@ -1,6 +1,18 @@
 import { DEFAULT_PRESSABLE_RIPPLE_COLOR } from '../constants';
 import { RenderHTMLSharedProps } from '../shared-types';
 
+function WebViewPlaceholder() {
+  /* istanbul ignore next */
+  if (__DEV__) {
+    console.warn(
+      'One of your renderers is attempting to use WebView component, which has not been ' +
+        "provided as a prop to the RenderHtml component. As a consequence, the element won't be rendered."
+    );
+  }
+  /* istanbul ignore next */
+  return null;
+}
+
 const defaultSharedProps: Required<RenderHTMLSharedProps> = {
   debug: false,
   defaultTextProps: {
@@ -11,17 +23,7 @@ const defaultSharedProps: Required<RenderHTMLSharedProps> = {
   enableExperimentalMarginCollapsing: false,
   computeEmbeddedMaxWidth: (contentWidth) => contentWidth,
   GenericPressable: undefined as any,
-  WebView: () => {
-    /* istanbul ignore next */
-    if (__DEV__) {
-      console.warn(
-        'One of your renderers is attempting to use WebView component, which has not been ' +
-          "provided as a prop to the RenderHtml component. As a consequence, the element won't be rendered."
-      );
-    }
-    /* istanbul ignore next */
-    return null;
-  },
+  WebView: WebViewPlaceholder,
   defaultWebViewProps: {},
   pressableHightlightColor: DEFAULT_PRESSABLE_RIPPLE_COLOR,
   customListStyleSpecs: {}
