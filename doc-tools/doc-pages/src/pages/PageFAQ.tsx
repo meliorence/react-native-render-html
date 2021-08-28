a43a89b909293f168c7dc3bfa033d126de2dc7bf
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-08-28 16:57:40 -0300

    docs: fix missing hyperlink

--- doc-tools/doc-pages/src/pages/PageFAQ.tsx
+++ doc-tools/doc-pages/src/pages/PageFAQ.tsx
@@ -114,7 +114,7 @@ export default function PageFAQ() {
         <Section title="Warning: You seem to update the X prop of the Y component in short periods of time...">
           <Paragraph>
             There is a detailed explaination for this warning here:
-            <Hyperlink url="">
+            <Hyperlink url="https://stackoverflow.com/q/68966120/2779871">
               Stack Overflow | react-native-render-html, "You seem to update
               ..."
             </Hyperlink>
