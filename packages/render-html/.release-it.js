be73d811d2418c632ac7b33247ae34f1759924e3
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-10-23 14:15:34 -0300

    chore: prepare 6.2.0 release

--- packages/render-html/.release-it.js
+++ packages/render-html/.release-it.js
@@ -2,7 +2,7 @@ module.exports = {
   git: {
     commitMessage: 'chore: release v${version}',
     // to release a changelog diff from a specific tag, uncoment
-    // changelog: 'git log --pretty=format:"* %s (%h)" v6.0.5...${to}'
+    changelog: 'git log --pretty=format:"* %s (%h)" v6.1.0...${to}'
   },
   npm: {
     publish: true
