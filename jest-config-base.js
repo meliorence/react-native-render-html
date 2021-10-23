e9af5a430ff29ee24245d648a2cbd1bb23978a9f
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-10-23 10:54:29 -0300

    ci(jest): fix base config

--- jest-config-base.js
+++ jest-config-base.js
@@ -4,6 +4,6 @@ module.exports = {
   testRegex: 'src/.*\\.test\\.tsx?$',
   coveragePathIgnorePatterns: ['/node_modules/', '__tests__'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|react-native-webview|ramda|react-native-render-html)/)'
+    'node_modules/(?!(react-native|react-native-webview|ramda|react-native-render-html|@react-native)/)'
   ]
 };
