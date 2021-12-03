e03b0acda71ac305f983d252875e34f035d605c0
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-12-03 07:25:06 -0300

    fix: wrong prop reference in warning message
    fix #541

--- packages/render-html/src/renderEmptyContent.ts
+++ packages/render-html/src/renderEmptyContent.ts
@@ -6,12 +6,12 @@ function renderEmptyContent({ tnode }: TNodeSubRendererProps<TNode>) {
     if (tnode.isUnregistered) {
       console.warn(
         `There is no custom renderer registered for tag "${tnode.tagName}" which is not part of the HTML5 standard. The tag will not be rendered.` +
-          ' If you don\'t want this tag to be rendered, add it to "ignoredTags" prop array. If you do, register an HTMLElementModel for this tag with "customHTMLElementModels" prop.'
+          ' If you don\'t want this tag to be rendered, add it to "ignoredDomTags" prop array. If you do, register an HTMLElementModel for this tag with "customHTMLElementModels" prop.'
       );
     } else if (tnode.tagName !== 'head') {
       console.warn(
         `The "${tnode.tagName}" tag is a valid HTML element but is not handled by this library. You must extend the default HTMLElementModel for this tag with "customHTMLElementModels" prop and make sure its content model is not set to "none".` +
-          ' If you don\'t want this tag to be rendered, add it to "ignoredTags" prop array.'
+          ' If you don\'t want this tag to be rendered, add it to "ignoredDomTags" prop array.'
       );
     }
   }
