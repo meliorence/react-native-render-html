b0d1232c2f2c7f5c5e227b86141c10eeb90a839a
bricezele <bricezele1@gmail.com>
2023-02-06 13:57:38 +0100

    Add support for Twitter, Youtube and Facebook Embed.

--- packages/render-html/src/RenderHTML.tsx
+++ packages/render-html/src/RenderHTML.tsx
@@ -1,9 +1,20 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, ReactNode, useState } from 'react';
+import {
+  Dimensions,
+  LayoutChangeEvent,
+  Platform,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {render} from 'posthtml-render';
+import {Node} from 'posthtml-parser';
+import WebView from 'react-native-webview';
 import { RenderHTMLProps } from './shared-types';
 import RenderHTMLDebug from './RenderHTMLDebug';
 import TRenderEngineProvider from './TRenderEngineProvider';
 import RenderHTMLConfigProvider from './RenderHTMLConfigProvider';
 import RenderHTMLSource from './RenderHTMLSource';
+import {RenderHtmlViewModel} from "./viewmodels/RenderHtmlViewModel";
 
 /**
  * Render HTML text in native views!
@@ -19,7 +30,34 @@ import RenderHTMLSource from './RenderHTMLSource';
  *
  * @public
  */
-export default function RenderHTML(props: RenderHTMLProps): ReactElement {
+
+interface RenderHtmlContentComponentProps extends RenderHTMLProps{
+  content: string
+}
+interface RenderHtmlElementProps {
+  element: Node | any
+  contentWidth?: number
+}
+
+const {width} = Dimensions.get('window')
+const vw = width / 100
+
+const styles = StyleSheet.create({
+  bannerAds: {alignItems: 'center', marginTop: 20},
+  blockQuoteContainer: {
+    paddingBottom: 0,
+    paddingRight: 10,
+    flexDirection: 'row',
+    marginTop: 15,
+    flex: 1,
+  },
+  blockQuoteContent: {
+    flex: 1,
+    width: vw * 80,
+  },
+})
+
+const RenderHtml: React.FC<RenderHTMLProps> = (props) => {
   const {
     source,
     onHTMLLoaded,
@@ -29,18 +67,146 @@ export default function RenderHTML(props: RenderHTMLProps): ReactElement {
     ...otherProps
   } = props;
   return (
-    <RenderHTMLDebug {...props}>
-      <TRenderEngineProvider {...otherProps}>
-        <RenderHTMLConfigProvider {...otherProps}>
-          {React.createElement(RenderHTMLSource, {
-            source,
-            onHTMLLoaded,
-            onTTreeChange,
-            onDocumentMetadataLoaded,
-            contentWidth
-          })}
-        </RenderHTMLConfigProvider>
-      </TRenderEngineProvider>
-    </RenderHTMLDebug>
+      <RenderHTMLDebug {...props}>
+        <TRenderEngineProvider {...otherProps}>
+          <RenderHTMLConfigProvider {...otherProps}>
+            {React.createElement(RenderHTMLSource, {
+              source,
+              onHTMLLoaded,
+              onTTreeChange,
+              onDocumentMetadataLoaded,
+              contentWidth
+            })}
+          </RenderHTMLConfigProvider>
+        </TRenderEngineProvider>
+      </RenderHTMLDebug>
+  )
+}
+
+const RenderHtmlElement: React.FC<RenderHtmlElementProps> = ({
+                                                               contentWidth,
+                                                               element,
+                                                               ...rest
+                                                             }) => {
+
+  /**
+   * METHODS
+   */
+  const onLayout = (event: LayoutChangeEvent) => {
+    return event.nativeEvent.layout.height
+  }
+
+  const renderBlockquote = () => (
+      <View style={[styles.blockQuoteContainer]}>
+        <View style={styles.blockQuoteContent} onLayout={onLayout}>
+          <RenderHtml
+              {...rest}
+              enableCSSInlineProcessing
+              contentWidth={width}
+              source={{
+                html: `<div style="
+                            font-size: 14;
+                            font-style: italic;
+                            line-height: 20;">
+                            ${render(element.node)}
+                        </div>`,
+              }}
+          />
+        </View>
+      </View>
+  )
+
+  return (
+      <React.Fragment>
+        {element.node.tag === 'blockquote' ? (
+            renderBlockquote()
+        ) : (
+            <RenderHtml
+                {...rest}
+                contentWidth={width}
+                source={{
+                  html: `${render(element.node)}`,
+                }}
+            />
+        )}
+      </React.Fragment>
+  )
+}
+
+const RenderHtmlContent: React.FC<RenderHtmlContentComponentProps> = ({
+                                                                        content,
+                                                                        readMore = null,
+                                                                      }) => {
+  /**
+   * INITIALIZATION
+   */
+  const [postVM] = useState(new RenderHtmlViewModel())
+  const [contentToNodeElement] = useState(
+      postVM.convertContentToNodeOrEmbed(content),
+  )
+
+  return (
+      <View style={{marginTop: postVM.hasSpecialTagToEmbed(content) ? 20 : 0}}>
+        {contentToNodeElement.map((element, index) => (
+            <React.Fragment key={index}>
+              {element.isEmbed ? (
+                  <WebView
+                      source={{
+                        html: `<script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>
+                                       <body style="display: flex; align-content: center; align-content: center;">${render(
+                            element.node,
+                        )}</body>`,
+                      }}
+                      style={{
+                        width,
+                        minHeight: postVM.isTwitterTag(element.node)
+                            ? 300
+                            : +element.node[0].attrs?.height / 1.8,
+                        resizeMode: 'cover',
+                        flex: 1,
+                        opacity: 0.99,
+                      }}
+                      userAgent={
+                        'Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/ 604.1.21 (KHTML, like Gecko) Version/ 12.0 Mobile/17A6278a Safari/602.1.26'
+                      }
+                      mediaPlaybackRequiresUserAction={false}
+                      renderToHardwareTextureAndroid={true}
+                      scrollEnabled={false}
+                      automaticallyAdjustContentInsets={false}
+                      androidLayerType={
+                        Platform.OS === 'android' &&
+                        Platform.Version <= 22
+                            ? 'hardware'
+                            : 'none'
+                      }
+                      injectedJavaScript={`const meta = document.createElement('meta'); meta.setAttribute('content', 'width=width, initial-scale=0.5, maximum-scale=0.5, user-scalable=2.0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `}
+                      bounces={false}
+                      javaScriptEnabled
+                      scalesPageToFit
+                      domStorageEnabled
+                      startInLoadingState
+                  />
+              ) : (
+                  <React.Fragment>
+                        <RenderHtmlElement
+                            contentWidth={width}
+                            element={element}
+                        />
+                  </React.Fragment>
+              )}
+            </React.Fragment>
+        ))}
+      </View>
+  )
+}
+
+
+export default function RenderHTML(props: RenderHtmlContentComponentProps): ReactElement {
+  const {
+    content,
+    ...otherProps
+  } = props;
+  return (
+      <RenderHtmlContent {...otherProps} content={content} />
   );
 }
---
+++ packages/render-html/src/viewmodels/RenderHtmlViewModel.tsx
@@ -0,0 +1,73 @@
+/**
+ * @Project react-native-render-html
+ * @File RenderHtmlViewModel.tsx
+ * @Path packages/render-html/src/viewmodels
+ * @Author BRICE ZELE
+ * @Date 06/03/2022
+ */
+
+import {decode} from 'html-entities'
+import {Node, parser} from 'posthtml-parser'
+
+interface NodeElement {
+    node: Node | any
+    isEmbed: boolean
+}
+
+export class RenderHtmlViewModel {
+
+    isBlockQuoteTag(node: Node): boolean {
+        if (node.hasOwnProperty('tag')) return node.tag === 'blockquote'
+        else return false
+    }
+
+    isTwitterTag(node: Node): boolean {
+        return (
+            this.isBlockQuoteTag(node) &&
+            node.attrs?.class?.includes('twitter-tweet')
+        )
+    }
+
+    isEmbedTag(node: Node): boolean {
+        if (node.hasOwnProperty('tag') && node.hasOwnProperty('content')) {
+            return (
+                node.content.filter((element) => element.tag === 'iframe')
+                    .length > 0
+            )
+        } else return false
+    }
+
+    hasSpecialTagToEmbed(content: string): boolean {
+        const htmlTree = parser(content)
+        const predicate = (node: Node) => {
+            return (
+                this.isTwitterTag(node) ||
+                this.isEmbedTag(node) ||
+                this.isBlockQuoteTag(node)
+            )
+        }
+
+        return htmlTree.some(predicate)
+    }
+
+    convertContentToNodeOrEmbed(content: string): Array<NodeElement> {
+        const htmlTree = parser(content)
+        let htmlNode: Array<NodeElement> = []
+
+        htmlTree.map((node) => {
+            if (this.isEmbedTag(node)) {
+                htmlNode.push({
+                    node: node.content,
+                    isEmbed: this.isEmbedTag(node),
+                })
+            } else {
+                htmlNode.push({
+                    node,
+                    isEmbed: this.isTwitterTag(node),
+                })
+            }
+        })
+
+        return htmlNode
+    }
+}
